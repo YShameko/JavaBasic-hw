66dcf69eeb11ab25323c1a40ebd4c1e911c8de6d
YShameko <yshameko@gmail.com>
2026-04-15 23:17:44 +0300

    hw04.1 with BigDecimal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,29 @@
 package org.example;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("When your income is 19 000, the tax will be " + calcIncomeTax(19000));
+        BigDecimal total = new BigDecimal("12153.41");
+        BigDecimal days = new BigDecimal("5");
+        printResults(1, "smartphone", days, total);
+
+        total = new BigDecimal("10486.85");
+        days = new BigDecimal("7");
+        printResults(2, "laptop", days, total);
     }
 
-    public static float calcIncomeTax (float income) {
-        float tax = 0.00f;
-        if (income >= 0 && income <= 10000) {
-            tax = income * 0.025f;
-        } else if (income > 10000 && income <= 25000) {
-            tax = income * 0.043f;
-        } else {
-            tax = income * 0.067f;
-        }
-
-        return tax;
+    private static BigDecimal calcDailySales(BigDecimal qtyDays, BigDecimal salesAmount) {
+        return salesAmount.divide(qtyDays, 2, RoundingMode.HALF_UP);
     }
 
+    private static void printResults (int productNo, String productName,
+                                      BigDecimal days, BigDecimal total) {
+        BigDecimal daily = calcDailySales(days, total);
+
+        System.out.printf("Product No %d: %s,%n", productNo, productName);
+        System.out.printf("total sales for %.0f days is EUR %.2f,%n", days, total);
+        System.out.printf("sales by day is EUR %.2f.%n", daily);
+    }
 }

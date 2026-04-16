b54e9e34a042b1d4a2d70dc868244a1409d62fd0
YShameko <yshameko@gmail.com>
2026-04-16 12:55:39 +0300

    hw06.1, using 'for'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,15 @@
 package org.example;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 public class Main {
     public static void main(String[] args) {
-        BigDecimal total = new BigDecimal("12153.41");
-        BigDecimal days = new BigDecimal("5");
-        printResults(1, "smartphone", days, total);
+        int sum = 0;
 
-        total = new BigDecimal("10486.85");
-        days = new BigDecimal("7");
-        printResults(2, "laptop", days, total);
-    }
+        for (int i = 1; i <= 6; i++) {
+            sum += i;
+            System.out.printf("%d) Num is %d, sum is %d %n", i, i, sum);
+        }
 
-    private static BigDecimal calcDailySales(BigDecimal qtyDays, BigDecimal salesAmount) {
-        return salesAmount.divide(qtyDays, 2, RoundingMode.HALF_UP);
-    }
-
-    private static void printResults (int productNo, String productName,
-                                      BigDecimal days, BigDecimal total) {
-        BigDecimal daily = calcDailySales(days, total);
-
-        System.out.printf("Product No %d: %s,%n", productNo, productName);
-        System.out.printf("total sales for %.0f days is EUR %.2f,%n", days, total);
-        System.out.printf("sales by day is EUR %.2f.%n", daily);
+        System.out.println("------------------------");
+        System.out.println("Sum of numbers is " + sum);
     }
 }

5ee0401e49a55e18cef4a2d61bb3b57400307fff
YShameko <yshameko@gmail.com>
2026-05-21 18:20:13 +0300

    hw15.1: ENUM, small changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Day todayIs = Day.THURSDAY;
-        TipOfTheDay myAdvise = new TipOfTheDay();
+        AdviseTips myAdvise = new TipOfTheDay();
 
         myAdvise.advise(todayIs);
     }
